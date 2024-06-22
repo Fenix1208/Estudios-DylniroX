6e7d483716ae4f01ecd8c87910fba8b333f875a5
fabian figueroa <85865806+Fenix1208@users.noreply.github.com>
2024-06-22 14:19:09 -0700

    version 1.6

--- src/Components/Footer/Footer.component.jsx
+++ src/Components/Footer/Footer.component.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 const Footer =()=>{
     return(
         <div className="contenedor-footer">
-            <p>Copyright ©2024 Cardinal de DyX v 1.5.2 -  Estudios DylniroX™ | DylniroX Software™ Derechos reservados. -- |RFFG|</p>
+            <p>Copyright ©2024 Cardinal de DyX v 1.6 -  Estudios DylniroX™ | DylniroX Software™ Derechos reservados. -- |RFFG|</p>
         </div>
     )
 };
